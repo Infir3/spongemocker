bd84c34dce02e6dc79b59157be74949a0752028b
Stefan Becking <stefanbecking@gmail.com>
2022-11-08 23:12:50 +0100

    format

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
-const inputArea = document.getElementById('inputArea');
-const outputArea = document.getElementById('outputArea');
-const clearBtn = document.getElementById('clearBtn');
+const inputArea = document.getElementById("inputArea");
+const outputArea = document.getElementById("outputArea");
+const clearBtn = document.getElementById("clearBtn");
 
 /**
  * @param {string} input
  * @returns {string} output
  */
 function spongemock(input) {
-    let output = '';
+  let output = "";
 
-    for (let i = 0; i < input.length; i++) {
-        const currentChar = input[i];
-        if (i % 2 === 0) {
-            output += currentChar.toLowerCase();
-        } else {
-            output += currentChar.toUpperCase();
-        }
+  for (let i = 0; i < input.length; i++) {
+    const currentChar = input[i];
+    if (i % 2 === 0) {
+      output += currentChar.toLowerCase();
+    } else {
+      output += currentChar.toUpperCase();
     }
+  }
 
-    return output;
+  return output;
 }
 
-inputArea.addEventListener('input', (val) => {
-    outputArea.value = spongemock(inputArea.value);
+inputArea.addEventListener("input", () => {
+  outputArea.value = spongemock(inputArea.value);
 });
 
-clearBtn.addEventListener('click', () => {
-    inputArea.value = '';
-    outputArea.value = '';
+clearBtn.addEventListener("click", () => {
+  inputArea.value = "";
+  outputArea.value = "";
 });
