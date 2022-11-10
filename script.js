8819af6cfac32d38809c37e48787dfe797a3c374
Stefan Becking <stefanbecking@gmail.com>
2022-11-10 20:29:57 +0100

    add copy to clipboard button (again)

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const inputArea = document.getElementById("inputArea");
 const outputArea = document.getElementById("outputArea");
+const copyBtn = document.getElementById("copyBtn");
 const clearBtn = document.getElementById("clearBtn");
 
 /**
@@ -23,6 +24,21 @@ function spongemock(input) {
 
 inputArea.addEventListener("input", () => {
   outputArea.value = spongemock(inputArea.value);
+});
+
+// instantiate ClipboardJS by passing the HTML element of the copy button
+new ClipboardJS(copyBtn);
+
+copyBtn.addEventListener("click", () => {
+  Toastify({
+    text: "Output copied to clipboard!",
+    duration: 2000,
+    gravity: "bottom", // `top` or `bottom`
+    position: "center", // `left`, `center` or `right`
+    style: {
+      background: "green",
+    },
+  }).showToast();
 });
 
 clearBtn.addEventListener("click", () => {
